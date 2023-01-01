bdcfb20d8d11aac7b2e30cd034e1089f7c6facb6
deBeee <fiacik12345@gmail.com>
2023-01-01 14:48:38 +0100

    Jakis drugi commit

--- src/main/java/pl/wszib/edu/shop/App.java
+++ src/main/java/pl/wszib/edu/shop/App.java
@@ -3,6 +3,7 @@ package pl.wszib.edu.shop;
 public class App {
     public static void main(String[] args) {
         System.out.printf("git!!!");
+        System.out.println("drugi commit!!!");
     }
 
 }
